e796f4a30b1ff5619f94233909ab895f23db2a0e
orenotan <ojamanegro@gmail.com>
2026-06-02 21:42:21 -0600

    Adding model-es

--- vpc/lib/miscellaneous/services/logic_service.dart
+++ vpc/lib/miscellaneous/services/logic_service.dart
@@ -1,16 +1,1 @@
-
-
 import 'dart:ffi';
-
-List<dynamic> test(String color, String option, String n){
-  if(color == "rojo"){
-    if(option == "gana"){
-      try{
-        //n.toInt (abs)
-      }catch{
-        //skip
-      }
-    }
-  }
-  return [];
-}
